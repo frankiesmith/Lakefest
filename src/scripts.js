a1f6a54a9325cca8b4b5593d502b49ad5edae0cf
Frankie Smith <frankie@5hdagency.com>
2020-02-25 17:57:39 -0500

    mobile menu edits/javascript added

---
+++ src/scripts.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function($) {
+	    /* Modal */
+$('.show').click(function(e){
+        $(e.currentTarget).siblings('.modal-overlay').addClass('active').find('.modal').addClass('active');
+    });
+
+    $('.close-modal, .modal-overlay').click(function(e){
+        $('.modal, .modal-overlay').removeClass('active');
+    });
+
+
+$(document).ready(function() {
+jQuery('header').on('click','a',function(e){
+  if(e.target.href.indexOf('#') !== -1 ){
+    e.preventDefault();
+    jQuery('html,body').animate(
+      {scrollTop: jQuery('#'+e.target.href.split('#')[1]).offset().top -90},
+      600
+    );
+  }
+  return true;
+		});
+});
+
+$(window).scroll(function() {
+		var scrollDistance = $(window).scrollTop() +320;
+	
+		// Assign active class to nav links while scolling
+		$('.menu-section').each(function(i) {
+				if ($(this).position().top <= scrollDistance) {
+						$('.desktop-menu a.active').removeClass('active');
+						$('.desktop-menu a').eq(i).addClass('active');
+				}
+		});
+        $('.menu-section').each(function(i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('.desktop-menu a.active').removeClass('active');
+            $('.desktop-menu a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
+
+});
